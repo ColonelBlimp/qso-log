0a4358d0cf303e8f9fce0980c27ad9efe0e58b0f
ColonelBlimp <ColonelBlimp@users.noreply.github.com>
2024-08-01 12:11:09 +0200

    Latest

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,20 +1,5 @@
 <script>
-    import Panel from "./components/Panel.svelte";
-    let name = 'Marc';
-    let src = 'assets/img/kaningna.jpg';
-    let gothtmlString = `This has <strong>html</strong> included!`;
-    let count = 0;
-    const increment = () => {
-        count++;
-    }
+    import StackedLayout from "./components/StackedLayout.svelte";
 </script>
 
-<div>Hello {name.toUpperCase()} test!</div>
-<div><img src="{src}" alt="{name} lives here..." class="w-48 h-48"></div>
-<Panel />
-<p>{@html gothtmlString}</p>
-
-<button on:click={increment}>
-    Clicked {count}
-    {count === 1 ? 'time' : 'times'}
-</button>
+<StackedLayout/>
---
+++ frontend/src/components/StackedLayout.svelte
@@ -0,0 +1,30 @@
+<div class="min-h-full">
+    <nav class="bg-indigo-600">
+        <div class="mx-auto max-w-7xl px-8">
+            <div class="flex h-16 items-center justify-between">
+                <div class="flex items-center">
+                    <div class="block">
+                        <div class="ml-10 flex items-baseline space-x-4">
+                            <a href="#" class="rounded-md bg-indigo-700 px-3 py-2 text-sm font-medium text-white" aria-current="page">Logbook</a>
+                            <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-indigo-500 hover:bg-opacity-75">Rig Control</a>
+                            <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-indigo-500 hover:bg-opacity-75">Rotator</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="-m-2 flex">
+                    <button type="button" class="relative inline-flex items-center justify-center rounded-md bg-indigo-600 p-2 text-indigo-200 hover:bg-indigo-500 hover:bg-opacity-75 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600" aria-controls="mobile-menu" aria-expanded="false">
+                        <span class="absolute -inset-0.5"></span>
+                        <!-- Menu open: "hidden", Menu closed: "block" -->
+                        <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                        </svg>
+                        <!-- Menu open: "block", Menu closed: "hidden" -->
+                        <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </nav>
+</div>
